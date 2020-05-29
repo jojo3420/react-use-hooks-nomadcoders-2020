5f8b6404318aa4130641ccd9a3636f3ba629d2a0
jojo3420 <jjjhhhvvv@gmail.com>
2020-05-29 13:42:08 +0900

    bugfix useInput 에서 사용 하지 않는 ref 제거

--- src/useInput/useInput.js
+++ src/useInput/useInput.js
@@ -2,15 +2,13 @@ import React, { useState, useRef, useEffect} from 'react';
 
 
 function UseInput() {
-  const ref = useRef();
   const [value, setValue] = useState('');
-
   const onChange = e => {
     const { target: {value}} = e;
     setValue(value);
   }
 
-  return { ref, value, onChange }
+  return { value, onChange }
 
 }
 
